e88e2032dab741d4374e7d554e7a35e6ef506e1c
niknyazev <n.knyazev.dev@gmail.com>
2022-02-28 20:11:12 +0300

    Add Demo Chart

--- WeightControl/ViewController.swift
+++ WeightControl/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    lazy var lineChartView: LineChartView = {
+        let chartView = LineChartView(frame: CGRect(x: 10, y: 10, width: 400, height: 400))
+        chartView.backgroundColor = .blue
+        return chartView
+    }()
+    
+    let points = [
+        ChartDataEntry(x: 10, y: 20),
+        ChartDataEntry(x: 20, y: 10),
+        ChartDataEntry(x: 30, y: 30),
+        ChartDataEntry(x: 40, y: 40),
+        ChartDataEntry(x: 50, y: 10)
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(lineChartView)
+        lineChartView.center = view.center
+        setChartData()
     }
 
+    private func setChartData() {
+        let dataSet = LineChartDataSet(entries: points, label: "Weight data")
+        let data = LineChartData(dataSet: dataSet)
+        lineChartView.data = data
+    }
+    
+
+}
+
+extension ViewController: ChartViewDelegate {
+    
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        
+    }
 
 }
 
