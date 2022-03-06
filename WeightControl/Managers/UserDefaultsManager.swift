235f13d3e5c53d71d125310ee73b15742ccf6b01
niknyazev <n.knyazev.dev@gmail.com>
2022-03-06 22:29:16 +0300

    Add Working with UserData

--- WeightControl/Managers/UserDefaultsManager.swift
+++ WeightControl/Managers/UserDefaultsManager.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct UserData {
+struct UserData: Codable {
     let age: Int
     let height: Int
     let weightGoal: Int
@@ -20,18 +20,22 @@ class UserDefaultsManager {
     static let shared = UserDefaultsManager()
     
     private let userDefaults = UserDefaults.standard
-    private let sortingKey = "userData"
+    private let key = "userData"
     
     private init() {}
     
     // MARK: - Public methods
     
-    func saveSortingType(sortingType: Int) {
-        userDefaults.set(sortingType, forKey: sortingKey)
+    func saveUserData(userData: UserData) {
+        guard let data = try? JSONEncoder().encode(userData) else { return }
+        userDefaults.set(data, forKey: key)
     }
     
-    func fetchSortingType() -> Int {
-        userDefaults.integer(forKey: sortingKey)
-    }
+    func fetchUserData() -> UserData? {
+        
+        guard let data = userDefaults.data(forKey: key) else { return nil }
+        guard let result = try? JSONDecoder().decode(UserData.self, from: data) else { return nil }
+        return result
     
+    }
 }
