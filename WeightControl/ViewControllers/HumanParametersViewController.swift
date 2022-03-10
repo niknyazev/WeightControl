7ae4c5464ae00448baa6fcb2109c54a108a620f4
niknyazev <n.knyazev.dev@gmail.com>
2022-03-10 18:27:11 +0300

    Add choosing age

--- WeightControl/ViewControllers/HumanParametersViewController.swift
+++ WeightControl/ViewControllers/HumanParametersViewController.swift
@@ -23,8 +23,39 @@ class HumanParametersViewController: UITableViewController {
         setupElements()
 
     }
+    
+    // MARK: - Table view
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        if indexPath.row == 0 {
+            selectAge()
+        }
+        
+    }
+    
     // MARK: - Private methods
+    
+    func selectAge() {
+        
+        let viewController = UIViewController()
+        viewController.preferredContentSize = CGSize(width: 250,height: 200)
+        
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: 250, height: 200))
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        viewController.view.addSubview(pickerView)
+        
+        let editRadiusAlert = UIAlertController(title: "Choose age", message: "", preferredStyle: .alert)
+        
+        editRadiusAlert.setValue(viewController, forKey: "contentViewController")
+        editRadiusAlert.addAction(UIAlertAction(title: "Done", style: .default))
+        editRadiusAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+       
+        self.present(editRadiusAlert, animated: true)
+        
+    }
     
     private func setupElements() {
         
@@ -39,6 +70,24 @@ class HumanParametersViewController: UITableViewController {
         sexLabel.text = userData.sex.rawValue
         weightGoalLabel.text = String(userData.weightGoal)
         
+    }
+
+}
+
+// MARK: - Picker view
+
+extension HumanParametersViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        String(row)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        100
     }
 
 }
