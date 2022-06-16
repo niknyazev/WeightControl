bbdd0c3b4b2261e466f3b9e5aa1a4386d2745217
niknyazev <n.knyazev.dev@gmail.com>
2022-06-16 21:57:41 +0300

    Add test file for StorageManager

---
+++ WeightControlTests/StorageManagerTests.swift
@@ -0,0 +1,41 @@
+//
+//  StorageManagerTests.swift
+//  UserDefaultsManagerTests
+//
+//  Created by Николай on 16.06.2022.
+//
+
+import XCTest
+@testable import WeightControl
+
+class StorageManagerTests: XCTestCase {
+
+//    private var sut: StorageManager!
+    
+    override func setUp() {
+        super.setUp()
+//        sut = StorageManager.shared
+//        sut.deleteAll()
+    }
+
+    override func tearDown() {
+//        sut = nil
+        super.tearDown()
+    }
+
+    func testSaveWeightData() throws {
+        
+//        let weightData = WeightData()
+//
+//        weightData.weightKilo = 100
+//        weightData.weightGramm = 20
+//        weightData.date = .now
+//        weightData.photoData = nil
+//
+//        sut.save(weightData)
+//
+//        let weightsData = sut.realm.objects(WeightData.self)
+//
+//        XCTAssertTrue(weightsData.count == 1, "Can't write weight data")
+    }
+}
